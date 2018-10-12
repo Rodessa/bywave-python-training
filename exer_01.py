500b684b8c49b93d4afdca8f079ea5f749926652
issa <issa@bywave.com.au>
2018-10-12 17:16:39 +0800

    Python  exercise 01

---
+++ exer_01.py
@@ -0,0 +1,15 @@
+print "Enter your number"
+x=range(60, 101)
+x=input()
+if x in range(60, 75):
+	print 'Derp!'
+elif x in range(75, 85):
+	print 'Good'
+elif x in range(85, 95):
+	print 'Very Good'
+elif x in range(95, 101):
+	print 'Level Asian'
+else:
+	print 'Invalid Value'
+
+#__Issa__ = '<issa@bywave.com.au>'
